3119f85ffdeef37a515342830d157224cee8b024
Lfahima <fahima.lakhi@stonybrook.edu>
2023-09-13 04:31:06 +0000

    I have updated

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 import pandas as pd
+import random
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def mainpage():
     return render_template('base.html')
+
+@app.route('/about')
+def aboutpage():
+    return render_template('about.html')
+
+@app.route('/random')
+def randomnumber():
+    number_var = random.randint(1, 10000)
+    return render_template('random.html', single_number = number_var)
 
 if __name__ == '__main__':
     app.run(
